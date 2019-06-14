f2636fe9dfe2a4e849f893597916c70b99624210
leipengfei_sx <532080984@qq.com>
2019-06-14 19:06:19 +0800

    11

--- src/main/java/com/example/demo/Controller/test.java
+++ src/main/java/com/example/demo/Controller/test.java
@@ -15,9 +15,40 @@ import java.util.Map;
 @Slf4j
 public class test {
 
+    @RequestMapping("/chart")
+    @ResponseBody
+    public Map chart(){
+
+        Map<String,Object> m=new HashMap<>();
+        Map<String,Object> xAxis=new HashMap<>();
+        Map<String,Object> yAxis=new HashMap<>();
+        Map<String,Object> series_list=new HashMap<>();
+        String type_xAxis="category";
+        String type_yAxis="value";
+        String type_series="line";
+        List<String>  data_xAxis=new ArrayList<>();
+        List<Integer>  data_series=new ArrayList<>();
+        List<Map>  series=new ArrayList<>();
+        for(int i=0;i<7;i++){
+            data_xAxis.add("Mon");
+            data_series.add(5);
+        }
+        xAxis.put("type",type_xAxis);
+        xAxis.put("data",data_xAxis);
+        yAxis.put("type",type_yAxis);
+        series_list.put("data",data_series);
+        series_list.put("type",type_series);
+        series.add(series_list);
+        m.put("xAxis",xAxis);
+        m.put("yAxis",yAxis);
+        m.put("series",series);
+        System.out.println(m);
+        return m;
+    }
+
     @RequestMapping("/test")
     public String testmethod(){
-        return "index.html";
+        return "chart.html";
     }
 
     @RequestMapping("/p")
@@ -29,20 +60,25 @@ public class test {
         m.put("take","22");
         m.put("Time","22");
         l.add(m);
+        Map<String,Object> m_all=new HashMap<>();
+        m_all.put("pagenum","5");
+        m_all.put("data",l);
         System.out.println(time);
         return l;
     }
 
     @RequestMapping("/q")
     @ResponseBody
-    public List q(String value){
+    public Map q(){
         List l= new ArrayList();
         Map<String,String> m=new HashMap<>();
         m.put("name","12");
         m.put("take","12");
         m.put("Time","12");
         l.add(m);
-        System.out.println(value);
-        return l;
+        Map<String,Object> m_all=new HashMap<>();
+        m_all.put("pagenum",5);
+        m_all.put("data",l);
+        return m_all;
     }}
 
---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,87 @@
+var serverAddress = "http://10.13.41.51:8080/luban-api";
+//var serverAddress = "http://localhost:9096";
+var videoListUrl = serverAddress + "/api/v1/web/videos/list?";
+var videoEditUrl = serverAddress + "/api/v1/web/videos/edit";
+var videoAddUrl = serverAddress + "/api/v1/web/videos/add";
+var videoDeleteUrl = serverAddress + "/api/v1/videos";
+
+
+function home() {
+    window.location.href = "home.html";
+}
+
+function loginPage() {
+    window.location.href = "login.html";
+}
+
+function authenticate(){
+    if(sessionStorage.getItem("currentUser")==null){
+        loginPage();
+        return false
+    }
+    return true;
+}
+
+function authorize() {
+    sessionStorage.setItem("currentUser", "admin");
+}
+
+function loginOut() {
+    if(sessionStorage.getItem("currentUser")!=null){
+        sessionStorage.setItem("currentUser", null);
+    }
+    window.location.href = "login.html";
+}
+
+//将输入串中的汉字变为UniCode(UTF8),其它字符不变
+function toUTF8(input) {
+    var wch, x, uch = "", output = "";
+    for (x = 0; x < input.length; x++) {
+        wch = input.charCodeAt(x);
+        if (!(wch & 0xFF80)) {
+            output += input.charAt(x);
+        } else if (!(wch & 0xF000)) {
+            uch = "%" + (wch >> 6 | 0xC0).toString(16) + "%" + (wch & 0x3F | 0x80).toString(16);
+            output += uch;
+        } else {
+            uch = "%" + (wch >> 12 | 0xE0).toString(16) + "%" + (((wch >> 6) & 0x3F) | 0x80).toString(16) + "%" + (wch & 0x3F | 0x80).toString(16);
+            output += uch;
+        }
+    }
+    return (output);
+}
+
+//将输入串中的UniCode(UTF8编码还原)
+function revertUTF8(input) {
+    var x, wch, wch1, wch2, uch = "", output = "";
+    for (x = 0; x < input.length; x++) {
+        if (input.charAt(x) == "%") {
+            wch = parseInt(input.charAt(++x) + input.charAt(++x), 16);
+            if (!wch) {
+                break;
+            }
+            if (!(wch & 0x80)) {
+                wch = wch;
+            } else if (!(wch & 0x20)) {
+                x++;
+                wch1 = parseInt(input.charAt(++x) + input.charAt(++x), 16);
+                wch = (wch & 0x1F) << 6;
+                wch1 = wch1 & 0x3F;
+                wch = wch + wch1;
+            } else {
+                x++;
+                wch1 = parseInt(input.charAt(++x) + input.charAt(++x), 16);
+                x++;
+                wch2 = parseInt(input.charAt(++x) + input.charAt(++x), 16);
+                wch = (wch & 0x0F) << 12;
+                wch1 = (wch1 & 0x3F) << 6;
+                wch2 = (wch2 & 0x3F);
+                wch = wch + wch1 + wch2;
+            }
+            output += String.fromCharCode(wch);
+        } else {
+            output += input.charAt(x);
+        }
+    }
+    return (output);
+}
